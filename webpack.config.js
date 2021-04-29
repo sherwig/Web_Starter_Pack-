e0c0a5b87620ff12b66bc039043ff650c34c291c
Sam <sahe2032@colorado.edu>
2021-04-29 11:05:02 -0600

    Way bettter

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
+// const path = require("path");
+// module.exports = {
+//   mode: "development",
+//   entry: "./src/index.js",
+//   output: {
+//     filename: "main.js",
+//     path: path.resolve(__dirname, "dist")
+//   },
+//   module: {
+//     rules: [{
+//       test: /\.scss$/,
+//       use: [
+//         "style-loader", // 3. injects css into DOM
+//         "css-loader", // 2.turns css into commonjs
+//         "sass-loader", // 1.turns sass into css
+//       ],
+//     }, ],
+//   },
+// };
+
+
 const path = require("path");
 module.exports = {
   mode: "development",
@@ -9,7 +30,6 @@ module.exports = {
   module: {
     rules: [{
         test: /\.scss$/,
-
         use: [
           "style-loader", // 3. injects css into DOM
           "css-loader", // 2.turns css into commonjs
